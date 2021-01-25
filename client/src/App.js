40f56d3c70a709acdc856c4d4cfe8bb4ebee22af
vappukoskela <vappu.koskela@gmail.com>
2021-01-25 15:54:19 +0200

    s10endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,8 @@ import strings from './localization/strings';
 import { useSnackbar, withSnackbar } from 'notistack';
 import socketIOClient from 'socket.io-client'
 import NewQuizDialog from './components/NewQuizDialog';
-const sIOEndpoint = 'ws://localhost:9000'
+const sIOEndpoint = 'ws://vappus-quiz-app.herokuapp.com:9000'
+// const sIOEndpoint = 'ws://localhost:9000'
 
 const useStyles = makeStyles((theme) => ({
   root: {
